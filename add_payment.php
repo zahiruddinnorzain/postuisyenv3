75998c3927872572414fd16d754a45084ef33dff
zahir <kepaknaga@gmail.com>
2020-01-23 15:00:34 +0800

    add calendar picker

--- add_payment.php
+++ add_payment.php
@@ -8,6 +8,13 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
+  <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+  <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+
+
 </head>
 <body>
 
@@ -33,7 +40,16 @@
 
     <div class="form-group">
       <label for="usr">Date to pay:</label>
-      <input type="text" class="form-control" id="pwd" name="ffPdatepay" required="true">
+      <!--<input type="text" class="form-control" id="pwd" name="ffPdatepay" required="true">-->
+
+      <input id="Pdatepay" name="ffPdatepay" width="276"  required="true" />
+        <script>
+          $('#Pdatepay').datepicker({
+              uiLibrary: 'bootstrap4',
+              format: 'dd/mm/yyyy',
+          });
+        </script>
+
     </div>
 
     <div class="form-group">
