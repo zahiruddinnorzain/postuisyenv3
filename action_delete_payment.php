e9673e5a3d7bac1a9a8fa6b26bfa208b405b4739
zahir <kepaknaga@gmail.com>
2020-01-23 14:35:35 +0800

    change back link on action page

--- action_delete_payment.php
+++ action_delete_payment.php
@@ -58,7 +58,7 @@ $conn->close();
 
 
       <br>
-      <a href="index2.php" class="btn btn-info btn-lg" role="button">Back</a>
+      <a href="list_payment.php" class="btn btn-info btn-lg" role="button">Back</a>
       <br><br>
 
     </div>
