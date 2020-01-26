f32d167b00b884cb659db938abc1eb26279334e8
zahir <kepaknaga@gmail.com>
2020-01-26 19:09:04 +0800

    add sql file

--- postuisyenv3.sql
+++ postuisyenv3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 21, 2020 at 09:50 AM
+-- Generation Time: Jan 26, 2020 at 12:05 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `payment` (
   `ID` int(11) NOT NULL,
+  `username` text NOT NULL,
   `Pnama` text NOT NULL,
   `Pdatepay` text NOT NULL,
   `Psubjek` text NOT NULL,
@@ -41,9 +42,9 @@ CREATE TABLE `payment` (
 -- Dumping data for table `payment`
 --
 
-INSERT INTO `payment` (`ID`, `Pnama`, `Pdatepay`, `Psubjek`, `Pjam`, `Pamount`) VALUES
-(7, 'zahir', '12-02-2020', 'math', 1, '30'),
-(8, 'Firdaus', '20-1-2020', 'Math, english, bm, science', 16, '400');
+INSERT INTO `payment` (`ID`, `username`, `Pnama`, `Pdatepay`, `Psubjek`, `Pjam`, `Pamount`) VALUES
+(13, 'zirah', 'adik', '27/01/2020', 'bm', 2, '60'),
+(15, 'zahir', 'adik ku', '28/01/2020', 'sains', 2, '60');
 
 -- --------------------------------------------------------
 
@@ -53,6 +54,7 @@ INSERT INTO `payment` (`ID`, `Pnama`, `Pdatepay`, `Psubjek`, `Pjam`, `Pamount`) 
 
 CREATE TABLE `pelajar` (
   `id` int(11) NOT NULL,
+  `username` text NOT NULL,
   `nama` text NOT NULL,
   `tahun_lahir` text NOT NULL,
   `subjek` text NOT NULL,
@@ -66,9 +68,35 @@ CREATE TABLE `pelajar` (
 -- Dumping data for table `pelajar`
 --
 
-INSERT INTO `pelajar` (`id`, `nama`, `tahun_lahir`, `subjek`, `hari`, `bayaranHour`, `alamat`, `nota`) VALUES
-(1, 'zahir', '1994', 'math', 'selasa', 30, 'no2 jln 5', 'abam'),
-(2, 'Firdaus', '2008', 'Math, english, science, malay', 'Tuesday, thursday', 25, 'Taman jati', '');
+INSERT INTO `pelajar` (`id`, `username`, `nama`, `tahun_lahir`, `subjek`, `hari`, `bayaranHour`, `alamat`, `nota`) VALUES
+(3, 'zahir', 'balong', '1994', 'math', 'isnin', 30, 'no2 lorong 4/6', 'along'),
+(4, 'zirah', 'adik', '2002', 'bm', 'rabu', 30, 'no2 lorong 4/8', 'test'),
+(5, 'zahir', 'mak', '1965', 'bm', 'isnin', 30, 'no2 lorong 4/1', 'mak');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teaching`
+--
+
+CREATE TABLE `teaching` (
+  `ID` int(11) NOT NULL,
+  `username` text NOT NULL,
+  `tnama` text NOT NULL,
+  `tsubjek` text NOT NULL,
+  `tdate` text NOT NULL,
+  `ttime` text NOT NULL,
+  `thours` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `teaching`
+--
+
+INSERT INTO `teaching` (`ID`, `username`, `tnama`, `tsubjek`, `tdate`, `ttime`, `thours`) VALUES
+(5, 'zahir', 'adik', 'bm', '28/01/2020', '12:12 pm', 2),
+(6, 'zahir', 'ayip', 'bi', '30/01/2020', '06:12 pm', 2),
+(8, 'zahir', 'adik', 'bm', '05/02/2020', '07:12 pm', 2);
 
 -- --------------------------------------------------------
 
@@ -80,15 +108,19 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(80) NOT NULL,
   `name` varchar(80) NOT NULL,
-  `password` varchar(80) NOT NULL
+  `password` varchar(80) NOT NULL,
+  `alamat` text NOT NULL,
+  `phone` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `name`, `password`) VALUES
-(1, 'zirah', 'norhazirah', 'tiaya');
+INSERT INTO `users` (`id`, `username`, `name`, `password`, `alamat`, `phone`) VALUES
+(1, 'zirah', 'norhazirah', 'tiaya', '', 0),
+(3, 'zahir', 'zahir', 'ctvk', 'loron 4/5, tmn muhibbah, 4800', 173323667),
+(4, 'ayip', 'ayip', '123', 'no.2 lorong 4/5', 17332443);
 
 --
 -- Indexes for dumped tables
@@ -107,6 +139,12 @@ ALTER TABLE `pelajar`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `teaching`
+--
+ALTER TABLE `teaching`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -120,19 +158,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `payment`
 --
 ALTER TABLE `payment`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `pelajar`
 --
 ALTER TABLE `pelajar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `teaching`
+--
+ALTER TABLE `teaching`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
